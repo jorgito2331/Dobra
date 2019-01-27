25fc8047379b9d86078108ba52ae1c85404d69df
jorge gaviria <jorge.gaviria@quipux.com>
2019-01-27 01:37:37 -0500

    Se realizan mejoras

--- src/test/java/automatizacion/contratista/crear.java
+++ src/test/java/automatizacion/contratista/crear.java
@@ -5,10 +5,77 @@
  */
 package automatizacion.contratista;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
 /**
  *
  * @author Usuario
  */
-public class crear {
+public class Crear {
+    WebDriver driver;
+    
+    //Generales
+    By irAContratistas = By.id("contratistas");
+    By btnAdicionar = By.id("crear");
+    
+    //Campos
+    By txtNombre = By.name("nombre");
+    By txtCorreo = By.name("correo");
+    By txtIdentificacion = By.name("identificacion");
+    By txtTelefono = By.name("telefono");
+    By txtDireccion = By.name("completa");
+    
+    //Selects
+    Select slcTipoId = new Select(driver.findElement(By.name("tipoId")));
+    
+    //Guardar
+    By btnGuardar = By.name("guar");
+    
+    public Crear(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickIrAContratistas() {
+        driver.findElement(irAContratistas).click();
+    }
+
+    public void clickBtnAdicionar() {
+        driver.findElement(btnAdicionar).click();
+    }
+
+    public void setTxtNombre(String nombre) {
+        driver.findElement(txtNombre).sendKeys(contratistaDTO.getNombre());
+    }
+
+    public void setTxtCorreo(String correo) {
+        driver.findElement(txtCorreo).sendKeys(contratistaDTO.getCorreo());
+    }
+
+    public void setTxtIdentificacion(String id) {
+        driver.findElement(txtIdentificacion).sendKeys(contratistaDTO.getIdentificacion());
+    }
+
+    public void setTxtTelefono(String Telefono) {
+        driver.findElement(txtTelefono).sendKeys(contratistaDTO.getTelefono());
+    }
+
+    public void clickTxtDireccion() {
+        driver.findElement(txtDireccion).click();
+    }
+
+    public void setSlcTipoId(String tipoId) {
+        slcTipoId.selectByValue(contratistaDTO.getTipoID());
+    }
+    
+    public void clickBtnGuardar() {
+        driver.findElement(btnGuardar).click();
+    }
+    
+    public void assertTest(){
+        Assert.assertTrue(driver.findElement(btnAdicionar).isDisplayed());
+    }
     
 }
